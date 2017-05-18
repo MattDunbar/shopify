74e07dd673ed71fa7ac6d55e0f06c99a83afd9d8
Matt Dunbar <matt@mattdsworld.com>
2017-05-19 02:23:27 +0700

    Initial Commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.application_name = "Shop The Look"
+  config.api_key = "a24152cde7846f348b878ad228eaf777"
+  config.secret = "9c8c3fead57e48d0b06ba38effcaf03f"
+  config.scope = "read_products"
+  config.embedded_app = true
+end
